ae4421259d216907f1c5e991833de267f0c37de7
aidenpantzer <aidenbpantzer@gmail.com>
2026-03-31 16:51:28 -0400

    Added @TeleOp annotation to DrivetrainDemo.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DrivetrainDemo.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name="Drivetrain Code")
+public class DrivetrainDemo extends OpMode {
+
+    // Declare the motors
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    // Declare the variables
+    double leftPow;
+    double rightPow;
+
+    @Override
+    public void init() {
+        // Define the motors using the hardware map stored on the driver hub
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        // Set the direction of the motors to account for mounting orientation
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
+    @Override
+    public void loop() {
+        leftPow = -gamepad1.left_stick_y;
+        rightPow = -gamepad1.right_stick_y;
+
+        frontLeft.setPower(leftPow);
+        backLeft.setPower(leftPow);
+        frontRight.setPower(rightPow);
+        backRight.setPower(rightPow);
+    }
+
+}
