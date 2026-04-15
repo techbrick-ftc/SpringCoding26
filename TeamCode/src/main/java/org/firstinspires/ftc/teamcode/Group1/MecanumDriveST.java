cda58c1c4dde2fc71219ec0005ca9606047afc1b
Shivam <swd3sh25@gmail.com>
2026-04-14 20:42:14 -0400

    This is my code for Mecanum Drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Group1/MecanumDriveST.java
@@ -0,0 +1,104 @@
+package org.firstinspires.ftc.teamcode.Group1;
+
+// Imports
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp
+public class MecanumDriveST extends OpMode {
+
+   private DcMotor frontLeft, frontRight, backLeft, backRight;
+   private IMU imu;
+
+    MecanumDriveST drive = new MecanumDriveST();
+
+    double forward, strafe, rotate;
+
+    public void init (){
+
+        // Declaring the motors
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        // The direction of the motors
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        // Encoder wire to keep the same speed no matter the obstruction.
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //Field centric
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        // Orientation of the Control Hub
+        RevHubOrientationOnRobot RevOrientation = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD);
+
+        // Initializing the IMU
+        imu.initialize(new IMU.Parameters(RevOrientation));
+
+        drive.init();
+    }
+
+
+    public void drive (double forward, double strafe, double rotate){
+
+        // Declaring the variables
+        double frontLeftPower = forward + strafe + rotate;
+        double frontRightPower = forward - strafe- rotate;
+        double backLeftPower = forward - strafe + rotate;
+        double backRightPower = forward + strafe - rotate;
+
+        // Restricting the drivetrain
+        double maxPower = 1.0;
+        //double maxSpeed = 1.0;
+
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(backLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(backRightPower));
+
+        //frontLeft.setPower(maxSpeed * (frontLeftPower / maxPower));
+
+    }
+
+    public void driveFieldRelative(double forward, double strafe, double rotate){
+        double theta = Math.atan2(forward, strafe);
+        double r = Math.hypot(strafe, forward);
+
+        theta = AngleUnit.normalizeRadians(theta -
+                imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS));
+
+        double newForward = r * Math.sin(theta);
+        double newStrafe = r * Math.cos(theta);
+
+        this.drive(newForward, newStrafe, rotate);
+
+    }
+
+    @Override
+    public void loop(){
+        forward = gamepad1.left_stick_y;
+        strafe = gamepad1.left_stick_x;
+        rotate = gamepad1.right_stick_x;
+
+        drive.driveFieldRelative(forward, strafe, rotate);
+    }
+}
+
+
